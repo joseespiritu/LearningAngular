bfca29644390ff035c294038f0c16e565346ce93
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-01-03 18:15:22 -0600

    Recibiendo parametros por URL, params and service

--- spa/src/app/components/heroe/heroe.component.ts
+++ spa/src/app/components/heroe/heroe.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { HeroesService } from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html'
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
 
-  constructor() { }
+  heroe:any = {};
 
-  ngOnInit(): void {
+  constructor( private activatedRoute:ActivatedRoute,
+                private _heroesServices:HeroesService
+    ) {
+    this.activatedRoute.params.subscribe( params => {
+      /* console.log(params['id']); */
+      this.heroe = this._heroesServices.getHeroe( params['id'] );
+      console.log(this.heroe);
+    });
   }
 
 }
