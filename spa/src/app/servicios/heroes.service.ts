eef5a2550b12c843851aaad7a26435d4701498ba
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-01-17 19:49:59 -0600

    Arreglando busqueda

--- spa/src/app/servicios/heroes.service.ts
+++ spa/src/app/servicios/heroes.service.ts
@@ -72,9 +72,13 @@ export class HeroesService {
         let heroeArr:Heroe[] = []
         termino = termino.toLowerCase();
 
-        for ( let heroe of this.heroes ){
+        for ( let i = 0; i < this.heroes.length; i++ ){
+
+            let heroe = this.heroes[i]
+
             let nombre = heroe.nombre.toLowerCase()
             if ( nombre.indexOf(termino) >= 0 ){
+                heroe.idx = i;
                 heroeArr.push(heroe);
             }
         }
@@ -89,4 +93,5 @@ export interface Heroe {
     img: string;
     aparicion: string;
     casa: string;
+    idx?: number;
 }
